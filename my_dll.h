5f445ab9de8727a984be05d325a67e1c9dc82015
ting1065 <fengtingtang1065@gmail.com>
2023-02-04 20:28:53 -0500

    modified the way of deciding the size of a player name

--- my_dll.h
+++ my_dll.h
@@ -93,7 +93,8 @@ int team_push_front(team_t* t, int roster, char* name){
         return 0;
     }
     newPlayer->rosterNum = roster;
-    newPlayer->name = (char*)malloc(sizeof(*name));
+    int nameSize = strlen(name);
+    newPlayer->name = (char*)malloc(sizeof(char) * (nameSize+1));
     *(newPlayer->name) = *name;
     newPlayer->next = t->head;
     newPlayer->previous = NULL;
@@ -128,7 +129,8 @@ int team_push_back(team_t* t, int roster, char* name){
         return 0;
     }
     newPlayer->rosterNum = roster;
-    newPlayer->name = (char*)malloc(sizeof(*name));
+    int nameSize = strlen(name);
+    newPlayer->name = (char*)malloc(sizeof(char) * (nameSize+1));
     *(newPlayer->name) = *name;
     newPlayer->next = NULL;
     newPlayer->previous = t->tail;
@@ -224,7 +226,8 @@ int team_insert(team_t* t, int pos, int roster, char* name){
         return 0;
     }
     newPlayer->rosterNum = roster;
-    newPlayer->name = (char*)malloc(sizeof(*name));
+    int nameSize = strlen(name);
+    newPlayer->name = (char*)malloc(sizeof(char) * (nameSize + 1));
     *(newPlayer->name) = *name;
     
     player_t* iterator = t->head;
