df1cb70dc99a258ac74e4548d363c2a6591e4282
ting1065 <fengtingtang1065@gmail.com>
2023-08-06 02:25:32 -0700

    modified comments for showing in public repository

--- dll_test.c
+++ dll_test.c
@@ -1,44 +1,10 @@
-// Note: This is not an exhaustive test suite, but gives you the idea
-//       of some tests you might want to create.
-//       Adding more tests and slowly making them more and more complicated
-//       as you develop your library is recommended.
-// 
-// Compile this assignment with: gcc -g -Wall dll_test.c -o dll_test
-//
-// Run with: ./dll_test
-// Then run with valgrind ./dll_test
-// And don't forget valgrind -s ./dll_test!
-//
-// Comment out tests that you're not ready to run yet
-// add them back in as you finish functions.
-// You can also add tests as you go to make sure
-// the functions you have written are working.
-//
-//
-// Include parts of the C Standard Library
-// These have been written by some other really
-// smart engineers.
-#include <stdio.h>  // For IO operations
-#include <stdlib.h> // for malloc/free
-#include <string.h> //for strings
-
-// Our library that we have written.
-// Also, by a really smart engineer!
+// test suite for the doubly linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "my_dll.h"
-// Note that we are locating this file
-// within the same directory, so we use quotations
-// and provide the path to this file which is within
-// our current directory.
-
-
-// A sample collection of tests for your program
-// We will be adding our own to test your program.
-
 
 // Tests creation and deletion of list
-// Ideally does not cause any segfaults, and this
-// is considered passing.
-// Dll should also be empty.
 int unitTest0(int status){
     int passed = 0;
     team_t* test = create_team();
@@ -84,9 +50,7 @@ int unitTest2(int status){
     return passed;
 }
 
-
-//NOTE: tests 3 and onward will segfault until
-//    you are returning a player node.  
+ 
 // Tests push back and pop_back function with 1 player
 int unitTest3(int status){
     int passed = 0;
@@ -659,12 +623,8 @@ int (*unitTests[])(int)={
 };
 
 
-// ====================================================
-// ================== Program Entry ===================
-// ====================================================
 int main(){
     unsigned int testsPassed = 0;
-    // List of Unit Tests to test your data structure
     int counter =0;
     while(unitTests[counter]!=NULL){
     	printf("========unitTest %d========\n",counter);
--- my_dll.h
+++ my_dll.h
@@ -1,23 +1,12 @@
-// =================== Support Code =================
-// Doubly Linked List ( Team DLL ).
+// ==================================================
+// Doubly Linked List
 //
-// - Implement each of the functions to create a working DLL that represents a hockey team.
-// - Do not change any of the function declarations
-//   - (i.e. team_t* create_team() should not have additional arguments)
-// - You should not have any 'printf' statements in your functions. 
-//   - (You may consider using these printf statements to debug,
-//     but they should be removed from your final version)
-//   - (You may write helper functions to help you debug your code such as print_list etc)
+// Create a working DLL that represents a hockey team.
 // ==================================================
 #ifndef MYDLL_H
 #define MYDLL_H
 
-// Create a player data structure to store data within
-// our Team DLL. In our case, we will store a hockey player in each player struct.
-// rosterNum is the player's roster number; roster numbers are always greater than 0
-// The longest player name ever used in the NHL is 14 characters, so you can 
-// assume the longest name you'll have to deal with is at most 20 character long
-// we are only using family names.
+// define a struct for the nodes of the DLL to represent a hockey player
 typedef struct player {
     int rosterNum;
     char* name;
@@ -25,10 +14,7 @@ typedef struct player {
     struct player* previous;
 } player_t;
 
-// Create a Team data structure that is a doubly linked list
-// Our Team DLL holds a pointer to the first player node in our Team dll called head,
-// and a pointer to the last player node in our Team called tail.
-// It also keeps count of how many active players we have on our Team
+// define a struct for the DLL to represent the whole hockey team
 typedef struct Team {
     int activePlayers;		// count keeps track of how many active players are on the Team.
     player_t* head;		// head points to the first player node in our DLL.
@@ -37,12 +23,9 @@ typedef struct Team {
 
 // Creates a Team 
 // Returns a pointer to a newly created Team.
-// The Team should be initialized with data on the heap.
-// (Think about what the means in terms of memory allocation)
-// The Team fields should also be initialized to default values.
 // Returns NULL if we could not allocate memory.
 team_t* create_team(){
-    // Modify the body of this function as needed.
+
     team_t* myTeam= (team_t*)malloc(sizeof(team_t));
     if (myTeam == NULL) {
         return NULL;
@@ -52,12 +35,11 @@ team_t* create_team(){
     myTeam->head = NULL;
     myTeam->tail = NULL;
 
-    return myTeam; // Note: This line is a 'filler' so the code compiles.
+    return myTeam;
 }
 
 
 
-// Team DLL Empty
 // Check if the team is empty
 // Returns -1 if the team is NULL.
 // Returns 1 if true (The team is completely empty)
@@ -74,14 +56,13 @@ int team_empty(team_t* t){
         return 0;
     }
 
-    return -1; // Note: This line is a 'filler' so the code compiles.
+    return -1; 
 }
 
 // push a new player to the front of the DLL ( before the first player in the list).
 // Returns -1 if DLL is NULL.
 // Returns 1 on success
-// Returns 0 on failure ( i.e. we couldn't allocate memory for the new player)
-// (i.e. the memory allocation for a new player failed).
+// Returns 0 on failure
 int team_push_front(team_t* t, int roster, char* name){
 
     if (t == NULL) {
@@ -115,16 +96,15 @@ int team_push_front(team_t* t, int roster, char* name){
 
     t->activePlayers++;
    
-    return 1; // Note: This line is a 'filler' so the code compiles.
+    return 1;
 }
 
 // push a new player to the end of the team DLL (after the last player in the list).
 // Returns -1 if DLL is NULL.
 // Returns 1 on success
-// Returns 0 on failure ( i.e. we couldn't allocate memory for the new player)
-// (i.e. the memory allocation for a new player failed).
+// Returns 0 on failure
 int team_push_back(team_t* t, int roster, char* name){
-	// TODO: Implement me!!
+	
     if (t == NULL) {
         return -1;
     }
@@ -156,15 +136,14 @@ int team_push_back(team_t* t, int roster, char* name){
 
     t->activePlayers++;
 
-    return 1; // Note: This line is a 'filler' so the code compiles.
+    return 1;
 }
 
 // Returns the first player in the DLL and also removes it from the team.
 // Returns NULL if the Team is NULL. 
 // Returns NULL on failure, i.e. there is no one to pop from the team.
-// Assume roster numbers are always greater than zero
 player_t* team_pop_front(team_t* t){
-    // TODO: Implement me!!
+    
     if (t == NULL) {
         return NULL;
     }
@@ -186,15 +165,14 @@ player_t* team_pop_front(team_t* t){
 	
     t->activePlayers--;
 
-    return temp; // Note: This line is a 'filler' so the code compiles.
+    return temp;
 }
 
 // Returns the last player in the Team, and also removes it from the list.
 // Returns NULL if the Team is NULL. 
-// Returns NULL on failure, i.e. there is no one to pop from the Team.
-// Assume roster numbers are always greater than zero
+// Returns NULL on failure.
 player_t* team_pop_back(team_t* t){
-    // TODO: Implement me!!
+    
     if (t == NULL) {
         return NULL;
     }
@@ -216,19 +194,15 @@ player_t* team_pop_back(team_t* t){
 
     t->activePlayers--;
     
-    return temp; // Note: This line is a 'filler' so the code compiles.
+    return temp;
 }
 
 // Inserts a new player before the player at the specified position.
 // Returns -1 if the list is NULL
 // Returns 1 on success
 // Retruns 0 on failure:
-//  * we couldn't allocate memory for the new player
-//  * we tried to insert at a negative location.
-//  * we tried to insert past the size of the list
-//   (inserting at the size should be equivalent of calling push_back).
 int team_insert(team_t* t, int pos, int roster, char* name){
-    // TODO: Implement me!!
+    
     if (t == NULL) {
         return -1;
     }
@@ -280,19 +254,14 @@ int team_insert(team_t* t, int pos, int roster, char* name){
 
     t->activePlayers++;
         
-    return 1; // Note: This line is a 'filler' so the code compiles.
+    return 1;
 }
 
 // Returns the roster number of the player at position pos starting at 0 
-// ( 0 being the first item )
 // Returns -1 if the list is NULL
-//  (does not remove the item)
 // Returns 0 on failure:
-//  * we tried to get at a negative location.
-//  * we tried to get past the size of the list
-// Assume roster numbers are always greater than zero
 int team_list_get(team_t* t, int pos){
-    // TODO: Implement me!!
+    
     if (t == NULL) {
         return -1;
     }
@@ -307,18 +276,14 @@ int team_list_get(team_t* t, int pos){
         iterator = iterator->next;
     }
 
-    return iterator->rosterNum; // Note: This line is a 'filler' so the code compiles.
+    return iterator->rosterNum;
 }
 
-// Removes the player at position pos starting at 0 ( 0 being the first item )
+// Removes the player at position pos starting at 0
 // Returns NULL if the list is NULL
 // Returns NULL on failure:
-//  * we tried to remove at a negative location.
-//  * we tried to remove get past the size of the list
-// Assume no negative numbers in the list or the number zero.
-// Otherwise returns the roster number of the player removed.
 player_t* team_list_remove(team_t* t, int pos){
-    // TODO: Implement me!!
+    
     if (t == NULL) {
         return NULL;
     }
@@ -353,30 +318,19 @@ player_t* team_list_remove(team_t* t, int pos){
 
     t->activePlayers--;
 
-    return iterator; // Note: This line is a 'filler' so the code compiles.
+    return iterator;
 }
 
 // Team Size
 // Returns -1 if the Team is NULL.
 // Queries the current size of a team
 int team_size(team_t* t){
-    // TODO: Implement me!!
+    
     if (t == NULL) {
         return -1;
     }
 
-    return t->activePlayers; // Note: This line is a 'filler' so the code compiles.
-}
-
-// BONUS function - not required for HW
-// no extra credit is given for this function
-// Team DLL Sort
-// sorts the team by roster number, putting the players in order
-// with the smallest roster number at the head of the list, 
-// in ascending order
-// only work on this if you're all done with everything else tested!!
-void team_sort(team_t* t){
-    // OPTIONAL
+    return t->activePlayers;
 }
 
 //Helper function
@@ -399,7 +353,7 @@ void free_player(player_t* p) {
 // Removes a Team and all of its players (...and their names!!...) from memory.
 // This should be called before the program terminates.
 void free_team(team_t* t){
-    // TODO: Implement me!!
+    
     if (t == NULL) {
         return;
     }
